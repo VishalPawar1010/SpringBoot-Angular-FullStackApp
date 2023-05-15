bb0bd22973289e21e6236d15c8e1db8a0adf3160
Vishal Pawar <vshalofficial@gmail.com>
2023-05-15 23:30:31 +0530

    added spring security

---
+++ source-code/ecommerce-project-release-1.0/01-basic-list/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/security/eComUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.luv2code.ecommerce.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.luv2code.ecommerce.dao.UserRepository;
+import com.luv2code.ecommerce.entity.User;
+
+public class eComUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.getUserByEmail(email);
+		if (user != null) {
+			return new eComUserDetails(user);
+		}
+		
+		throw new UsernameNotFoundException("Could not find user with email: " + email);
+	}
+
+}
