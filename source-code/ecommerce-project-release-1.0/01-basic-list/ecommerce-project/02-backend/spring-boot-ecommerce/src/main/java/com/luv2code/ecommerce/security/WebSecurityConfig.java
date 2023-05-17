bb0ff02a42e27676aaf77ea8143876c9671cce65
Vishal Pawar <vshalofficial@gmail.com>
2023-05-17 14:47:30 +0530

    added angular login page and authGuard

--- source-code/ecommerce-project-release-1.0/01-basic-list/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/security/WebSecurityConfig.java
+++ source-code/ecommerce-project-release-1.0/01-basic-list/ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/security/WebSecurityConfig.java
@@ -64,11 +64,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //				.loginPage("/login")
 //				.usernameParameter("email")
 //				.permitAll()
-////			.and().logout().permitAll()
-////			.and()
-////				.rememberMe()
-////					.key("AbcDefgHijKlmnOpqrs_1234567890")
-////					.tokenValiditySeconds(7 * 24 * 60 * 60)
+//			.and().logout().permitAll()
+//			.and()
+//				.rememberMe()
+//					.key("AbcDefgHijKlmnOpqrs_1234567890")
+//					.tokenValiditySeconds(7 * 24 * 60 * 60)
 					;
 					
 			
--- source-code/ecommerce-project-release-1.0/01-basic-list/ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ source-code/ecommerce-project-release-1.0/01-basic-list/ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { RolesListComponent } from './components/roles-list/roles-list.component';
@@ -11,22 +11,35 @@ import { UserService } from './services/user.service';
 import { RolesService } from './services/roles.service';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { AuthenticateGuard } from './authenticate.guard';
+import { RequestInterceptor } from './request.interceptor';
 
 const routes: Routes = [
-  { path: 'products', component: ProductListComponent },
-  { path: 'users', component: UserListComponent },
-  { path: 'roles', component: RolesListComponent },
-  { path: 'category/:id', component: ProductListComponent },
-  { path: 'category', component: ProductListComponent },
+  {
+    path: '',
+    canActivate: [AuthenticateGuard],
+    children: [
+      // { path: '', component: UserListComponent },
+      { path: 'login', component: LoginComponent },
+      { path: '**', redirectTo: '' },
+      { path: 'products', component: ProductListComponent },
+      { path: 'users', component: UserListComponent },
+      { path: 'roles', component: RolesListComponent },
+      { path: 'category/:id', component: ProductListComponent },
+      { path: 'category', component: ProductListComponent },
 
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: '**', redirectTo: '/products', pathMatch: 'full' },
-  // { path: 'users/:id', component: UserListComponent },
-  // { path: 'roles/:id', component: UserListComponent },
-  // { path: 'roles/:id', component: RolesListComponent },
+      { path: '', redirectTo: '/products', pathMatch: 'full' },
+      { path: '**', redirectTo: '/products', pathMatch: 'full' },
+      // { path: 'users/:id', component: UserListComponent },
+      // { path: 'roles/:id', component: UserListComponent },
+      // { path: 'roles/:id', component: RolesListComponent },
 
-  // { path: '', redirectTo: '/users', pathMatch: 'full' },
-  // { path: '**', redirectTo: '/users', pathMatch: 'full' },
+      // { path: '', redirectTo: '/users', pathMatch: 'full' },
+      // { path: '**', redirectTo: '/users', pathMatch: 'full' },
+    ],
+  },
 ];
 @NgModule({
   declarations: [
@@ -35,9 +48,20 @@ const routes: Routes = [
     UserListComponent,
     RolesListComponent,
     ProductCategoryMenuComponent,
+    LoginComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
-  providers: [ProductService, UserService, RolesService],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+  ],
+  providers: [
+    ProductService,
+    UserService,
+    RolesService,
+    { provide: HTTP_INTERCEPTORS, useClass: RequestInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
